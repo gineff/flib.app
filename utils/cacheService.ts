3784f0b904db97d63cb9752694dc952e826c88d1
gineff <canone@inbox.ru>
2024-12-17 20:16:05 +0300

    ft: add cache service

---
+++ utils/cacheService.ts
@@ -0,0 +1,39 @@
+import * as Crypto from 'expo-crypto'
+import { storage } from '@/storage'
+
+export class CacheService {
+  private static instance: CacheService
+  constructor() {
+    if (!CacheService.instance) {
+      CacheService.instance = this
+    }
+    return CacheService.instance
+  }
+
+  private async hashUrl(url: string): Promise<string> {
+    const hashed = await Crypto.digestStringAsync(
+      Crypto.CryptoDigestAlgorithm.SHA256,
+      url
+    )
+    return hashed
+  }
+
+  async fetchWithCache<T>(
+    url: string,
+    fetcher: () => Promise<T[]>
+  ): Promise<T[]> {
+    const urlHash = await this.hashUrl(url)
+
+    const cachedData = await storage.getItem(urlHash)
+    if (cachedData) {
+      console.log(`Fetching data from cache for URL: ${url}`)
+      return JSON.parse(cachedData) as T[]
+    }
+
+    const data = await fetcher()
+
+    await storage.setItem(urlHash, JSON.stringify(data))
+    console.log(`Fetching data from network for URL: ${url}`)
+    return data
+  }
+}

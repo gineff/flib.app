11143e5652c5b21f501ecaff212a6bd5589487c7
gineff <canone@inbox.ru>
2024-12-20 22:35:33 +0300

    fix: add url update

--- utils/fetchService.ts
+++ utils/fetchService.ts
@@ -3,41 +3,43 @@ import { CacheService } from './cacheService'
 
 export class FetchService<T> {
   private url: string | null
+  private resourceBaseUrl: string
   private cacheService: CacheService
 
   constructor(initialPath: string) {
     this.url = initialPath
+    this.resourceBaseUrl = this.getResourceBaseUrl(initialPath)
     this.cacheService = new CacheService()
   }
-  private async fetchRequest(url: string): Promise<T[]> {
+  private getResourceBaseUrl = (path: string) =>
+    path.split('/').slice(0, -1).join('/')
+  private async request(url: string): Promise<PageData<T>> {
     const response = await fetch(url)
 
     if (!response.ok) {
       throw new Error(`Failed to fetch data: ${response.statusText}`)
     }
 
-    const body: PageData<T> = await response.json()
-
-    const { data, next } = body
-    this.url = next ?? null
-
-    return data
+    return response.json()
   }
 
   async *fetchData(): AsyncGenerator<T> {
     const initUrl = this.url
 
     while (this.url) {
-      const data =
+      const pageData =
         initUrl === this.url
-          ? await this.fetchRequest(this.url)
-          : await this.cacheService.fetchWithCache(this.url, () =>
-              this.fetchRequest(this.url!)
+          ? await this.request(this.url)
+          : await this.cacheService.fetchWithCache<PageData<T>>(
+              `${this.url}`,
+              () => this.request(this.url!)
             )
-
+      const { data, next } = pageData
       for (const item of data) {
         yield item
       }
+
+      this.url = next ? `${this.resourceBaseUrl}/${next}` : null
     }
   }
 }
